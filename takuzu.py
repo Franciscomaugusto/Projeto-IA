84e62f98e45fc277552d70f8cabc9755256713ec
Franciscomaugusto <72284885+Franciscomaugusto@users.noreply.github.com>
2022-05-17 23:11:33 +0100

    Add files via upload

---
+++ takuzu.py
@@ -0,0 +1,128 @@
+# takuzu.py: Template para implementação do projeto de Inteligência Artificial 2021/2022.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 00:
+# 00000 Nome1
+# 00000 Nome2
+
+import sys
+from search import (
+    Problem,
+    Node,
+    astar_search,
+    breadth_first_tree_search,
+    depth_first_tree_search,
+    greedy_search,
+    recursive_best_first_search,
+)
+
+
+class TakuzuState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = TakuzuState.state_id
+        TakuzuState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    # TODO: outros metodos da classe
+
+
+def list_creation(number):
+    lst = [[]] * number
+    for i in range(n):
+        lst[i] = [] * number
+    return lst
+
+
+class Board:
+    """Representação interna de um tabuleiro de Takuzu."""
+    positions = [[]]
+    max = 0
+
+    def get_number(self, row: int, col: int) -> int:
+        """Devolve o valor na respetiva posição do tabuleiro."""
+        # TODO
+        pass
+
+    def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
+        """Devolve os valores imediatamente abaixo e acima,
+        respectivamente."""
+        # TODO
+        pass
+
+    def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
+        """Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente."""
+        # TODO
+        pass
+
+    @staticmethod
+    def parse_instance_from_stdin():
+        """Lê o test do standard input (stdin) que é passado como argumento
+        e retorna uma instância da classe Board.
+
+        Por exemplo:
+            $ python3 takuzu.py < input_T01
+
+            > from sys import stdin
+            > stdin.readline()
+        """
+        self.max = int(sys.stdin.readline())
+        temp = list_creation(self.max)
+        for i in range(self.max):
+            line = sys.stdin.readline()
+            row = [int(s) for s in line.split() if s.isdigit()]
+            temp[i] = row
+        self.positions = temp
+
+
+    # TODO: outros metodos da classe
+
+
+class Takuzu(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        # TODO
+        pass
+
+    def actions(self, state: TakuzuState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        # TODO
+        pass
+
+    def result(self, state: TakuzuState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+        # TODO
+        pass
+
+    def goal_test(self, state: TakuzuState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas com uma sequência de números adjacentes."""
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro de input de sys.argv[1],
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    pass
