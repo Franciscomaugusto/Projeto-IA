f668e96239d638df4547b2e4cf993d5ea3fb4ed8
Franciscomaugusto <72284885+Franciscomaugusto@users.noreply.github.com>
2022-05-22 23:01:09 +0100

    Update takuzu.py

--- takuzu.py
+++ takuzu.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 12:
+# 00000 Francisco Augusto
+# 00000 Luis Marques
 
 import sys
 
